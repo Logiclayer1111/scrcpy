Logiclayer1111/scrcpy

commit ef75042473a1a121a74ff2849ec0f6b3a10adf74

Copy codec parameters from context
Now that the recorder have access to the codec context, it may
automatically initialize the stream codec parameters.

The V4L2 sink could do the same.

app/src/v4l2_sink.c
@@ -205,9 +205,11 @@ sc_v4l2_sink_open(struct sc_v4l2_sink *vs, const AVCodecContext *ctx) {
         goto error_avformat_free_context;
     }
 
-    ostream->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
-    ostream->codecpar->codec_id = encoder->id;
-    ostream->codecpar->format = AV_PIX_FMT_YUV420P;
+    int r = avcodec_parameters_from_context(ostream->codecpar, ctx);
+    if (r < 0) {
+        goto error_avformat_free_context;
+    }
+
     ostream->codecpar->width = vs->frame_size.width;
     ostream->codecpar->height = vs->frame_size.height;
 

app/src/recorder.c
@@ -473,9 +473,12 @@ sc_recorder_video_packet_sink_open(struct sc_packet_sink *sink,
         return false;
     }
 
-    stream->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
-    stream->codecpar->codec_id = ctx->codec->id;
-    stream->codecpar->format = AV_PIX_FMT_YUV420P;
+    int r = avcodec_parameters_from_context(stream->codecpar, ctx);
+    if (r < 0) {
+        sc_mutex_unlock(&recorder->mutex);
+        return false;
+    }
+
     stream->codecpar->width = recorder->declared_frame_size.width;
     stream->codecpar->height = recorder->declared_frame_size.height;
 
@@ -554,15 +557,11 @@ sc_recorder_audio_packet_sink_open(struct sc_packet_sink *sink,
         return false;
     }
 
-    stream->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
-    stream->codecpar->codec_id = ctx->codec->id;
-#ifdef SCRCPY_LAVU_HAS_CHLAYOUT
-    stream->codecpar->ch_layout.nb_channels = 2;
-#else
-    stream->codecpar->channel_layout = AV_CH_LAYOUT_STEREO;
-    stream->codecpar->channels = 2;
-#endif
-    stream->codecpar->sample_rate = 48000;
+    int r = avcodec_parameters_from_context(stream->codecpar, ctx);
+    if (r < 0) {
+        sc_mutex_unlock(&recorder->mutex);
+        return false;
+    }
 
     recorder->audio_stream_index = stream->index;
 

commit 65a888b03bf456c384b55a57a9b292aede94b16b

Fix v4l2 sink
The codec id to write as codec parameters is the one from the v4l2
encoder, not from the decoder.

Regression introduced by ef75042473a1a121a74ff2849ec0f6b3a10adf74.

Fixes #3795 <https://github.com/Genymobile/scrcpy/issues/3795>

app/src/v4l2_sink.c
@@ -210,6 +210,9 @@ sc_v4l2_sink_open(struct sc_v4l2_sink *vs, const AVCodecContext *ctx) {
         goto error_avformat_free_context;
     }
 
+    // The codec is from the v4l2 encoder, not from the decoder
+    ostream->codecpar->codec_id = encoder->id;
+
     int ret = avio_open(&vs->format_ctx->pb, vs->device_name, AVIO_FLAG_WRITE);
     if (ret < 0) {
         LOGE("Failed to open output device: %s", vs->device_name);